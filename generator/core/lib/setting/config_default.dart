8bff99768e370982b651fd9a1609026f1cc2b403
usayuki <masapp.01a@gmail.com>
2023-05-19 18:39:29 +0900

    Update config_default.dart

--- generator/core/lib/setting/config_default.dart
+++ generator/core/lib/setting/config_default.dart
@@ -4,10 +4,5 @@ name: UNKNOWN
 flutter_alternate_icon:
   # Optional
   output: lib/gen/
-
-flutter:
-  # See: https://flutter.dev/docs/development/ui/assets-and-images#specifying-assets
-  assets: []
-  # See: https://flutter.dev/docs/cookbook/design/fonts#2-declare-the-font-in-the-pubspec
-  fonts: []
+  asset: assets/app_icons/
 ''';
